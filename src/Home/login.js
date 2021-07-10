91e93c6f98eadc9265e13cafac97773ec3b30395
Sai Akhil <75438614+akki4134@users.noreply.github.com>
2021-07-10 23:22:29 +0530

    Update login.js

--- src/Home/login.js
+++ src/Home/login.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import './Styles/login.css'
@@ -34,6 +35,29 @@ const useStyles = makeStyles((theme) => ({
 
 const Login = () => {
 
+    const [username, setusername] = useState()
+    const [password, setpassword] = useState()
+
+    const signin = async() => {
+
+        console.log(username, password)
+
+        
+        let result = await fetch("http://localhost:8080/api/user/login", {
+            method: 'POST',
+            header: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(username, password)
+        })
+        result = await result.json()
+        localStorage.setItem("user-info", JSON.stringify(result))
+        // history.push("/blogHome")
+
+    }
+
+
     const classes = useStyles()
 
     return (
@@ -44,9 +68,9 @@ const Login = () => {
                         <img src="https://cdn1.vectorstock.com/i/1000x1000/48/55/logo-letter-a-glitch-distortion-diagonal-vector-22854855.jpg" id="icon" height="50px" width="10px" alt="User Icon" />
                     </div> */}
                     <form>
-                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="username" />
-                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
-                        <input type="submit" className="fadeIn fourth" value="Log In" />
+                        <input type="text" onChange={(e) => setusername(e.target.value)} id="login" className="fadeIn second" name="login" placeholder="username" />
+                        <input type="password" onChange={(e) => setpassword(e.target.value)} id="password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="button" onClick={() => signin()} className="fadeIn fourth" value="Log In" />
                     </form>
                 </div>
             </div>
